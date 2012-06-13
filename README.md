185870a3f92224f9c1759f79af18ed8554dfdd88
Stefan Lindecke <github.com@chektrion.de>
2012-06-13 14:51:09 +0200

    - Angebundene Domains - kleinere Anpassungen

--- README.md
+++ README.md
@@ -9,4 +9,24 @@ Beispiel
 	print_r($uberspace->arrValue);
 
 	
-Generiert ein Array, mit dem Buntzernamen, der zugeorndete Host, dem eingestellten Wunschpreis und dem Guthaben auf dem Account
+Generiert ein Array, mit dem Buntzernamen, der zugeorndete Host, angebundene Web- und MailDomains, dem eingestellten Wunschpreis und dem Guthaben auf dem Account
+
+	Array
+	(
+        [username]   =>   USERNAME
+        [wunschpreis]   =>   3.00
+        [guthaben]   =>   2.00
+        [hostname]   =>   HOST.uberspace.de
+        [domains_webserver]   =>   Array
+                (
+                        [0]   =>   *.DOMAIN.TLD
+                        [1]   =>   DOMAIN.TLD
+                        [2]   =>   *.USERNAME.HOST.uberspace.de
+                )
+
+        [domains_mailserver]   =>   Array
+                (
+                        [0]   =>   DOMAIN.TLD
+                )
+
+	)
--- classUberspaceWebRequest.php
+++ classUberspaceWebRequest.php
@@ -1,145 +1,145 @@
 <?php
 
 class classUberspaceWebRequest
-	{
-		function __construct($username, $password)
-		{
-			$this->strCookieFile = "cookie_".time().".txt";
-			
-			libxml_use_internal_errors(true);
-			setlocale(LC_ALL, 'de_DE'); 
-		
-		
-			$this -> arrValue = array();
-			$this -> ch = curl_init();
+        {
+                function __construct($username, $password)
+                {
+                        $this->strCookieFile = "cookie_".time().".txt";
 
-			curl_setopt($this -> ch, CURLOPT_POST, 1);
-			curl_setopt($this -> ch, CURLOPT_USERAGENT, "classUberspaceWebRequest");
+                        libxml_use_internal_errors(true);
+                        setlocale(LC_ALL, 'de_DE');
 
-			curl_setopt($this -> ch, CURLOPT_COOKIEJAR, $this->strCookieFile);
-			curl_setopt($this -> ch, CURLOPT_COOKIEFILE, $this->strCookieFile);
-			curl_setopt($this -> ch, CURLOPT_FOLLOWLOCATION, true);
-			curl_setopt($this -> ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($this -> ch, CURLOPT_HEADER, 1);
-			curl_setopt($this -> ch, CURLOPT_SSL_VERIFYHOST, 0);
-			curl_setopt($this -> ch, CURLOPT_SSL_VERIFYPEER, 0);
-		
-			$strUrl = 'https://uberspace.de/login';
-			$strData = sprintf("login=%s&password=%s&submit=anmelden", $username, $password);
 
-			$this -> arrValue['username'] = $username;
+                        $this -> arrValue = array();
+                        $this -> ch = curl_init();
 
-			curl_setopt($this -> ch, CURLOPT_URL, $strUrl);
-			curl_setopt($this -> ch, CURLOPT_POSTFIELDS, $strData);
-		}
+                        curl_setopt($this -> ch, CURLOPT_POST, 1);
+                        curl_setopt($this -> ch, CURLOPT_USERAGENT, "classUberspaceWebRequest");
 
-		function request()
-		{
-			$this -> store = curl_exec($this -> ch);
+                        curl_setopt($this -> ch, CURLOPT_COOKIEJAR, $this->strCookieFile);
+                        curl_setopt($this -> ch, CURLOPT_COOKIEFILE, $this->strCookieFile);
+                        curl_setopt($this -> ch, CURLOPT_FOLLOWLOCATION, true);
+                        curl_setopt($this -> ch, CURLOPT_RETURNTRANSFER, 1);
+                        curl_setopt($this -> ch, CURLOPT_HEADER, 1);
+                        curl_setopt($this -> ch, CURLOPT_SSL_VERIFYHOST, 0);
+                        curl_setopt($this -> ch, CURLOPT_SSL_VERIFYPEER, 0);
 
-			$this->getTransactions();
-			$this->getSettings();
-			$this->getDomains();
-		}
+                        $strUrl = 'https://uberspace.de/login';
+                        $strData = sprintf("login=%s&password=%s&submit=anmelden", $username, $password);
 
-		function execute($page)
-		{
-			curl_setopt($this -> ch, CURLOPT_URL, $page);
-			$this -> content = curl_exec($this -> ch);
-		}
+                        $this -> arrValue['username'] = $username;
 
-		function close()
-		{	
-			curl_close($this -> ch);
-			if (file_exists($this->strCookieFile))
-				unlink($this->strCookieFile);
-				
-				
-			libxml_use_internal_errors(false);
-		}
+                        curl_setopt($this -> ch, CURLOPT_URL, $strUrl);
+                        curl_setopt($this -> ch, CURLOPT_POSTFIELDS, $strData);
+                }
 
-		function getTransactions()
-		{
-			$this->execute('https://uberspace.de/dashboard/accounting');
-			
-			$doc = new DomDocument();
-			$doc->validateOnParse = false;
-			$doc->strictErrorChecking = false;
-			$doc->loadHTML(trim($this->content));
+                function request()
+                {
+                        $this -> store = curl_exec($this -> ch);
 
-			$id = $doc->getElementById('accounting_price');
+                        $this->getTransactions();
+                        $this->getSettings();
+                        $this->getDomains();
+                }
 
-			if ($id!=null)
-			{
-				$this -> arrValue['wunschpreis'] = $this->floatval($id->getAttribute("value"));
-				$idTotal = $doc->getElementById('total');
-				$this -> arrValue['guthaben'] = $this->floatval($idTotal->nodeValue);
-			}
-		}
+                function execute($page)
+                {
+                        curl_setopt($this -> ch, CURLOPT_URL, $page);
+                        $this -> content = curl_exec($this -> ch);
+                }
 
-		
-		function getDomains()
-		{
-			$this -> execute('https://uberspace.de/dashboard/domain');
-			
-			$doc = new DomDocument();
-			$doc->validateOnParse = false;
-			$doc->strictErrorChecking = false;
-			$doc->loadHTML($this->content);
+                function close()
+                {
+                        curl_close($this -> ch);
+                        if (file_exists($this->strCookieFile))
+                                unlink($this->strCookieFile);
 
-			$arrTags = $doc->getElementsByTagName('div');
-			
-			$arrFetchTags = array("Mailserver","Webserver");
 
-			foreach ($arrTags as $node)
-			{
-				if ($node->getAttribute("class")=="c300 fl")
-				{
-					$component = $node->getElementsByTagName('h2');
-					
-					foreach ($component as $comp)
-					{
-						if (in_array(trim($comp->nodeValue),$arrFetchTags))
-						{
-							$arrDomains = $node->getElementsByTagName('li');
-							
-							foreach ($arrDomains as $domain)
-							{
-								$this->arrValue['domains_'.strtolower(trim($comp->nodeValue))][] = trim($domain->nodeValue);
-							}
-							
-						}
-					}
-				}
-			}
-		}
-		function getSettings()
-		{
-			$this -> execute('https://uberspace.de/dashboard/datasheet');
-			$strContent = nl2br($this -> content);
+                        libxml_use_internal_errors(false);
+                }
 
-			preg_match('#<th>Hostname</th>(.*)uberspace.de<br />#s', $strContent, $matches);
+                function getTransactions()
+                {
+                        $this->execute('https://uberspace.de/dashboard/accounting');
 
-			if (is_array($matches))
-			{
-				$this -> arrValue['hostname'] = trim(strip_tags($matches[1] . 'uberspace.de'));
-			}
+                        $doc = new DomDocument();
+                        $doc->validateOnParse = false;
+                        $doc->strictErrorChecking = false;
+                        $doc->loadHTML(trim($this->content));
 
-		}
+                        $id = $doc->getElementById('accounting_price');
 
-		
-		function floatval($strValue) 
-		{ 
-			$floatValue = preg_replace("@(^[0-9]*)(\\.|,)([0-9]*)(.*)@", "\\1.\\3", $strValue); 
-			if (!is_numeric($floatValue)) 
-				$floatValue = preg_replace("@(^[0-9]*)(.*)@", "\\1", $strValue); 
-			if (!is_numeric($floatValue)) 
-				$floatValue = 0; 
-				
-			return $floatValue; 
-		} 
-  
-  
-	}
+                        if ($id!=null)
+                        {
+                                $this -> arrValue['wunschpreis'] = $this->floatval($id->getAttribute("value"));
+                                $idTotal = $doc->getElementById('total');
+                                $this -> arrValue['guthaben'] = $this->floatval($idTotal->nodeValue);
+                        }
+                }
+
+
+                function getDomains()
+                {
+                        $this -> execute('https://uberspace.de/dashboard/domain');
+
+                        $doc = new DomDocument();
+                        $doc->validateOnParse = false;
+                        $doc->strictErrorChecking = false;
+                        $doc->loadHTML($this->content);
+
+                        $arrTags = $doc->getElementsByTagName('div');
+
+                        $arrFetchTags = array("Mailserver","Webserver");
+
+                        foreach ($arrTags as $node)
+                        {
+                                if ($node->getAttribute("class")=="c300 fl")
+                                {
+                                        $component = $node->getElementsByTagName('h2');
+
+                                        foreach ($component as $comp)
+                                        {
+                                                if (in_array(trim($comp->nodeValue),$arrFetchTags))
+                                                {
+                                                        $arrDomains = $node->getElementsByTagName('li');
+
+                                                        foreach ($arrDomains as $domain)
+                                                        {
+                                                                $this->arrValue['domains_'.strtolower(trim($comp->nodeValue))][] = trim($domain->nodeValue);
+                                                        }
+
+                                                }
+                                        }
+                                }
+                        }
+                }
+                function getSettings()
+                {
+                        $this -> execute('https://uberspace.de/dashboard/datasheet');
+                        $strContent = nl2br($this -> content);
+
+                        preg_match('#<th>Hostname</th>(.*)uberspace.de<br />#s', $strContent, $matches);
+
+                        if (is_array($matches))
+                        {
+                                $this -> arrValue['hostname'] = trim(strip_tags($matches[1] . 'uberspace.de'));
+                        }
+
+                }
+
+
+                function floatval($strValue)
+                {
+                        $floatValue = preg_replace("@(^[0-9]*)(\\.|,)([0-9]*)(.*)@", "\\1.\\3", $strValue);
+                        if (!is_numeric($floatValue))
+                                $floatValue = preg_replace("@(^[0-9]*)(.*)@", "\\1", $strValue);
+                        if (!is_numeric($floatValue))
+                                $floatValue = 0;
+
+                        return $floatValue;
+                }
+
+
+        }
 
 ?>
